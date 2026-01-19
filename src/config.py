8d8ad7fefd3521fb5b2cf12794551837ec13f317
Aneesh Gidda <aneeshgidda@gmail.com>
2026-01-19 03:26:17 -0500

    refactor: migrate config to toml

--- src/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent
-RAW_DATA_PATH = ROOT_DIR / "data" / "dataset.csv"
-FEATURES_DIR = ROOT_DIR / "models" / "features"
-ARTIFACTS_DIR = ROOT_DIR / "models" / "artifacts"
